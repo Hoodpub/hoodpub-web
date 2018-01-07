f0c73870fe45c600cb4f5866c576d3b83172ab33
Hyunmin Choi <pointer81@gmail.com>
2018-01-07 16:16:38 +0900

    Add readme

--- README.md
+++ README.md
@@ -25,3 +25,16 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Todo
+
+| 제목                      | 설명                                      |
+| -------------             | -------------                             |
+| Getting data from backend | 백엔드와 연동하여 실제 데이타 가지고 오기 |
+| Infinite scrolling        | 갱신된 내용 받아오기                      |
+| Starting to test          | 테스트 코드 삽입을 위한 환경 이해         |
+
+
+## 진행
+
+2018.1.7 - 국립중앙도서관
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule,
+import { MatButtonModule, MatToolbarModule, MatMenuModule,
          MatIconModule, MatCardModule, MatGridListModule } from '@angular/material';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule, MatMenuModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule, MatMenuModule],
 })
 export class MaterialModule { }
